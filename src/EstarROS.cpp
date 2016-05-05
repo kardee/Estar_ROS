d4694afeee674fd432b4139f55135687f1bd14a2
Karthikeyan Deenadayalan <karthikeyand1992@gmail.com>
2016-05-05 19:12:00 +0200

    Update EstarROS.cpp

--- src/EstarROS.cpp
+++ src/EstarROS.cpp
@@ -1,3 +1,16 @@
+/*
+/* ROS-CHOMP.
+ *
+ * Copyright (C) 2016 KARTHIKEYAN. All rights reserved.
+ * License (3-Cluase BSD): https://github.com/kardee/Estar_ROS
+ *
+ * This code uses and is based on code from:
+ *   Project: ESTAR_ROS https://https://github.com/poftwaresatent/estar2
+ *   Copyright (C) 2014 Roland Philippsen. All rights reserved.
+ *   License (3-Clause BSD) : https://github.com/poftwaresatent/trychomp
+ * ***/
+
+
 #include "EstarROS.h"
 #include <ros/ros.h>
 #include <pluginlib/class_list_macros.h>
