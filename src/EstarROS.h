db325eb18423d12b2f4572c68ab0e4044bcdf92f
Karthikeyan Deenadayalan <karthikeyand1992@gmail.com>
2016-05-05 19:13:29 +0200

    Update EstarROS.h

--- src/EstarROS.h
+++ src/EstarROS.h
@@ -1,3 +1,14 @@
+/*
+/* ROS-ESTAR.
+ *
+ * Copyright (C) 2016 KARTHIKEYAN. All rights reserved.
+ * License (3-Cluase BSD): https://github.com/kardee/Estar_ROS
+ *
+ * This code uses and is based on code from:
+ *   Project: ESTAR_ROS https://https://github.com/poftwaresatent/estar2
+ *   Copyright (C) 2014 Roland Philippsen. All rights reserved.
+ *   License (3-Clause BSD) : https://github.com/poftwaresatent/estar2
+ * ***/
 #include <iostream>
 #include <cstdio>
 #include <stdlib.h>
